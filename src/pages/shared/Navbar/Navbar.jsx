a3392409bff997c322ac480d73df933fc772638b
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-11-14 19:01:18 +0600

    made the website title dynamic and changed navbar section

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
+
+    const navLinks = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+    </>
+
+
     return (
         <div className="navbar bg-black bg-opacity-40 fixed z-10 container mx-auto">
             <div className="navbar-start text-white">
@@ -9,32 +17,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">Bristo Boss</a>
             </div>
             <div className="navbar-center hidden lg:flex text-white">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 text-black">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navLinks}
                 </ul>
             </div>
             <div className="navbar-end">
